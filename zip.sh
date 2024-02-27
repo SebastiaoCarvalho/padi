3cae93c6bcc7c9283bd310daf972172f62799711
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-02-27 16:28:24 +0000

    add script to export homework

---
+++ zip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+group="group45" # Change this to your group number
+folder=$1
+pdf=$1.pdf
+name="padi-$1-$group"
+zip=$name.zip
+
+# Create a zip file
+cp $folder/$pdf .
+cp $folder/*.py .
+
+# Zip the folder
+zip $zip $pdf *.py
+
+# Remove the files
+rm -rf $pdf *.py
